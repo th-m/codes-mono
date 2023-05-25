00ec6d4ea973bf482fe056d7112ab9c23e27e926
th-m <thomvaladez@gmail.com>
2023-05-25 17:39:47 -0600

    try this garbage

---
+++ apps/home/netlify/assets/index.d039ee74.js
@@ -0,0 +1,1 @@
+import{g as t,t as e}from"./entry-client.9d21cc98.js";const s=e('<main class="text-center h-full mx-auto text-gray-50 p-4 bg-slate-900"><h1 class="max-6-xs text-6xl font-thin uppercase my-16">th-m.codes</h1><div class="flex flex-row justify-center"><img src="/images/th_m-bald.webp">');function l(){return t(s)}export{l as default};
---
+++ apps/home/netlify/assets/slack.7d3ac50e.js
@@ -0,0 +1,1 @@
+import{g as e,i as a,c as n,t as r}from"./entry-client.9d21cc98.js";import{A as s}from"./AppInstall.6af89568.js";const o=r('<main class="text-center mx-auto text-gray-700 p-4">');function m(){return(()=>{const t=e(o);return a(t,n(s,{app:"slack"})),t})()}export{m as default};
