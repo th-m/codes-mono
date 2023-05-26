8654da4571790e7e87f1fec752fcb6e81b9c7940
th-m <thomvaladez@gmail.com>
2023-05-25 18:33:03 -0600

    adjust it again

--- apps/home/tailwind.config.cjs
+++ apps/home/tailwind.config.cjs
@@ -8,7 +8,6 @@ module.exports = {
       __dirname,
       'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,svelte,ts,tsx,vue}'
     ),
-    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {
